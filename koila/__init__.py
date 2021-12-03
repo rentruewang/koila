fbafedf116a6a86c64d384112f59d2039d9c567e
RenChu Wang <patrick1031wang@gmail.com>
2021-12-03 16:02:42 +0800

    🔄 Prepared for some refactors!
    Added a preview of EagerTensor, like LazyTensor but evaluates eagerly. This used to simplify API significantly in the future, removing a lot of isinstance checks.

--- koila/__init__.py
+++ koila/__init__.py
@@ -1,5 +1,13 @@
 from . import constants, gpus
+from .eager import EagerTensor
 from .errors import UnsupportedError
-from .interfaces import BatchInfo, BatchNoBatch, Runnable, RunnableTensor, TensorMixin
+from .interfaces import (
+    BatchedPair,
+    BatchInfo,
+    Runnable,
+    RunnableTensor,
+    TensorMixin,
+    run,
+)
+from .lazy import Evaluation, LazyFunction, LazyTensor, lazy
 from .prepasses import CallBack, MetaData, PrePass, PrePassFunc
-from .tensors import Evaluation, LazyFunction, LazyTensor, lazy, run
