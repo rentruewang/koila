1c828aa5ac65553e9d6eb010c9742e988c8f21f9
RenChu Wang <patrick1031wang@gmail.com>
2022-01-02 19:11:25 +0800

    ➕ Added a few functions so runnables and tensors work together.
    `delayed`, `tensors`, `runnables` now pass the check.

--- koila/tensors/runnables.py
+++ koila/tensors/runnables.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 from abc import abstractmethod
-from typing import Protocol, TypeVar, runtime_checkable
+from typing import Callable, NamedTuple, Protocol, TypeVar, runtime_checkable
+
+from .tensors import TensorLike
 
 T = TypeVar("T", covariant=True)
 
@@ -10,4 +12,24 @@ T = TypeVar("T", covariant=True)
 class Runnable(Protocol[T]):
     @abstractmethod
     def run(self) -> T:
+        ...
+
+
+class BatchedPair(NamedTuple):
+    batch: int
+    no_batch: int
+
+
+class BatchInfo(NamedTuple):
+    index: int
+    value: int
+
+    def map(self, func: Callable[[int], int]) -> BatchInfo:
+        index = func(self.index)
+        return BatchInfo(index, self.value)
+
+
+class RunnableTensor(Runnable[TensorLike], Protocol):
+    @abstractmethod
+    def run(self, partial: range | None = None) -> TensorLike:
         ...
