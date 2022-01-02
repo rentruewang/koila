131b06a1da72a767516e238922c51bb5791205c5
RenChu Wang <patrick1031wang@gmail.com>
2022-01-02 18:15:14 +0800

    ⚙️ Added `pyproject.toml` as per PEP 518.
    Also, bug fix for `TensorLike` class.

--- koila/tensors/tensors.py
+++ koila/tensors/tensors.py
@@ -3,11 +3,14 @@ from __future__ import annotations
 import functools
 import operator
 from abc import abstractmethod
-from typing import NoReturn, Protocol, Tuple, overload
+from typing import Any, NoReturn, Protocol, Tuple, TypeVar, overload
 
 from torch import Tensor
 from torch import device as Device
 from torch import dtype as DType
+
+Number = TypeVar("Number", int, float)
+Numeric = TypeVar("Numeric", int, float, bool)
 
 
 class TensorLike(Protocol):
@@ -18,9 +21,7 @@ class TensorLike(Protocol):
     dtype: DType
     device: str | Device
 
-    @abstractmethod
-    def run(self, partial: Tuple[int, int] | None = None) -> Tensor:
-        ...
+    data: TensorLike
 
     @abstractmethod
     def __str__(self) -> str:
@@ -28,198 +29,215 @@ class TensorLike(Protocol):
 
     @abstractmethod
     def __bool__(self) -> bool:
-        ...
+        return bool(self.item())
 
     @abstractmethod
     def __int__(self) -> int:
-        ...
+        return int(self.item())
 
     @abstractmethod
     def __float__(self) -> float:
-        ...
+        return float(self.item())
 
     @abstractmethod
-    def __invert__(self) -> bool:
-        ...
+    def __invert__(self) -> TensorLike:
+        return self.logical_not()
+
+    @abstractmethod
+    def logical_not(self) -> TensorLike:
+        return not self
 
     @abstractmethod
     def __pos__(self) -> TensorLike:
-        ...
+        return self.pos()
+
+    @abstractmethod
+    def pos(self) -> TensorLike:
+        return +self
 
     @abstractmethod
     def __neg__(self) -> TensorLike:
-        ...
+        return self.neg()
+
+    @abstractmethod
+    def neg(self) -> TensorLike:
+        return -self
 
     @abstractmethod
     def __add__(self, other: TensorLike) -> TensorLike:
-        ...
+        return TensorLike.add(self, other)
 
     @abstractmethod
     def __radd__(self, other: TensorLike) -> TensorLike:
-        ...
+        return TensorLike.add(other, self)
+
+    @abstractmethod
+    def add(self: TensorLike, other: TensorLike) -> TensorLike:
+        return self + other
 
     @abstractmethod
     def __sub__(self, other: TensorLike) -> TensorLike:
-        ...
+        return TensorLike.sub(self, other)
 
     @abstractmethod
     def __rsub__(self, other: TensorLike) -> TensorLike:
-        ...
+        return TensorLike.sub(other, self)
+
+    @abstractmethod
+    def sub(self: TensorLike, other: TensorLike) -> TensorLike:
+        return self - other
+
+    subtract = sub
 
     @abstractmethod
     def __mul__(self, other: TensorLike) -> TensorLike:
-        ...
+        return TensorLike.mul(self, other)
 
     @abstractmethod
     def __rmul__(self, other: TensorLike) -> TensorLike:
-        ...
+        return TensorLike.mul(other, self)
+
+    @abstractmethod
+    def mul(self: TensorLike, other: TensorLike) -> TensorLike:
+        return self * other
+
+    multiply = mul
 
     @abstractmethod
     def __truediv__(self, other: TensorLike) -> TensorLike:
-        ...
+        return self.div(other)
 
     @abstractmethod
     def __rtruediv__(self, other: TensorLike) -> TensorLike:
-        ...
+        return other.div(self)
 
-    @abstractmethod
     def __floordiv__(self, other: TensorLike) -> TensorLike:
-        ...
+        raise NotImplementedError
 
-    @abstractmethod
     def __rfloordiv__(self, other: TensorLike) -> TensorLike:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
-    def __pow__(self, other: TensorLike) -> TensorLike:
-        ...
-
-    @abstractmethod
-    def __rpow__(self, other: TensorLike) -> TensorLike:
-        ...
-
-    @abstractmethod
-    def __mod__(self, other: TensorLike) -> TensorLike:
-        ...
-
-    @abstractmethod
-    def __rmod__(self, other: TensorLike) -> TensorLike:
-        ...
-
-    @abstractmethod
-    def __divmod__(self, other: TensorLike) -> NoReturn:
-        ...
-
-    @abstractmethod
-    def __rdivmod__(self, other: TensorLike) -> NoReturn:
-        ...
-
-    @abstractmethod
-    def __abs__(self) -> TensorLike:
-        ...
-
-    @abstractmethod
-    def __hash__(self) -> int:
-        ...
-
-    @abstractmethod
-    def __matmul__(self, other: TensorLike) -> NoReturn:
-        ...
-
-    @abstractmethod
-    def __rmatmul__(self, other: TensorLike) -> NoReturn:
-        ...
-
-    @abstractmethod
-    def __eq__(self, other: TensorLike) -> NoReturn:
-        ...
-
-    @abstractmethod
-    def __ne__(self, other: TensorLike) -> NoReturn:
-        ...
-
-    @abstractmethod
-    def __gt__(self, other: TensorLike) -> NoReturn:
-        ...
-
-    @abstractmethod
-    def __ge__(self, other: TensorLike) -> NoReturn:
-        ...
-
-    @abstractmethod
-    def __lt__(self, other: TensorLike) -> NoReturn:
-        ...
-
-    @abstractmethod
-    def __le__(self, other: TensorLike) -> NoReturn:
-        ...
-
-    @abstractmethod
-    def __len__(self) -> int:
-        ...
-
-    @abstractmethod
-    def add(self, other: TensorLike) -> TensorLike:
-        ...
-
-    @abstractmethod
-    def sub(self, other: TensorLike) -> TensorLike:
-        ...
-
-    @abstractmethod
-    def mul(self, other: TensorLike) -> TensorLike:
-        ...
-
-    @abstractmethod
-    def div(self, other: TensorLike) -> TensorLike:
-        ...
+    def div(self: TensorLike, other: TensorLike) -> TensorLike:
+        return self / other
 
     divide = truediv = div
 
     @abstractmethod
-    def pow(self, other: TensorLike) -> TensorLike:
-        ...
+    def __pow__(self, other: TensorLike) -> TensorLike:
+        return self.pow(other)
+
+    @abstractmethod
+    def __rpow__(self, other: TensorLike) -> TensorLike:
+        return TensorLike.pow(other, self)
+
+    @abstractmethod
+    def pow(self: TensorLike, other: TensorLike) -> TensorLike:
+        return self ** other
+
+    @abstractmethod
+    def __mod__(self, other: TensorLike) -> TensorLike:
+        return self.mod(other)
+
+    @abstractmethod
+    def __rmod__(self, other: TensorLike) -> TensorLike:
+        return other.mod(self)
+
+    def mod(self, other: TensorLike) -> TensorLike:
+        return self % other
+
+    fmod = remainder = mod
+
+    def __divmod__(self, other: TensorLike) -> NoReturn:
+        raise NotImplementedError
+
+    def __rdivmod__(self, other: TensorLike) -> NoReturn:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __abs__(self) -> TensorLike:
+        return self.abs()
 
     @abstractmethod
     def abs(self) -> TensorLike:
-        ...
+        return abs(self)
+
+    def __hash__(self) -> int:
+        return id(self)
+
+    @abstractmethod
+    def __matmul__(self, other: TensorLike) -> TensorLike:
+        return self.matmul(other)
+
+    @abstractmethod
+    def __rmatmul__(self, other: TensorLike) -> TensorLike:
+        return other.matmul(self)
 
     @abstractmethod
     def matmul(self, other: TensorLike) -> TensorLike:
-        ...
+        return self @ other
 
     @abstractmethod
-    def eq(self, other: TensorLike) -> TensorLike:
-        return not self.ne(other)
+    def __eq__(self, other: TensorLike | Numeric) -> TensorLike:
+        return self.eq(other)
 
     @abstractmethod
-    def ne(self, other: TensorLike) -> TensorLike:
-        return not self.eq(other)
+    def eq(self, other: TensorLike | Numeric) -> TensorLike:
+        return self == other
+
+    @abstractmethod
+    def __ne__(self, other: TensorLike) -> TensorLike:
+        return self.ne(other)
+
+    @abstractmethod
+    def ne(self, other: TensorLike | Numeric) -> TensorLike:
+        return self != other
+
+    @abstractmethod
+    def __gt__(self, other: TensorLike) -> TensorLike:
+        return self.gt(other)
 
     @abstractmethod
     def gt(self, other: TensorLike) -> TensorLike:
-        ...
+        return self > other
+
+    @abstractmethod
+    def __ge__(self, other: TensorLike) -> NoReturn:
+        return self.ge(other)
 
     @abstractmethod
     def ge(self, other: TensorLike) -> TensorLike:
-        ...
+        return self >= other
+
+    @abstractmethod
+    def __lt__(self, other: TensorLike) -> NoReturn:
+        return self.lt(other)
 
     @abstractmethod
     def lt(self, other: TensorLike) -> TensorLike:
-        ...
+        return self < other
+
+    @abstractmethod
+    def __le__(self, other: TensorLike) -> NoReturn:
+        return self.le(other)
 
     @abstractmethod
     def le(self, other: TensorLike) -> TensorLike:
-        ...
+        return self <= other
+
+    @abstractmethod
+    def __len__(self) -> int:
+        return self.size(0)
 
     @abstractmethod
     def dim(self) -> int:
         return len(self.size())
 
     @property
-    @abstractmethod
     def ndim(self) -> int:
         return self.dim()
+
+    ndimension = ndim
 
     @overload
     @abstractmethod
@@ -236,15 +254,20 @@ class TensorLike(Protocol):
         ...
 
     @property
-    @abstractmethod
     def shape(self) -> Tuple[int, ...]:
         return self.size()
 
-    @abstractmethod
     def numel(self) -> int:
-        return functools.reduce(operator.mul, self.shape)
+        return functools.reduce(operator.mul, self.shape, 1)
+
+    @abstractmethod
+    def item(self) -> Numeric:
+        ...
+
+    @abstractmethod
+    def transpose(self, dim0: int, dim1: int) -> TensorLike:
+        ...
 
     @property
-    @abstractmethod
     def T(self) -> TensorLike:
-        ...
+        return self.transpose(0, 1)
