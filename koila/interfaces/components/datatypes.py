421f083ad3e7a99258b9f612a114c2adf0a17570
RenChu Wang <patrick1031wang@gmail.com>
2022-02-01 05:08:34 +0800

    📄 Wrote documentations for interfaces.
    This makes interfaces' responsibility clear.

--- koila/interfaces/components/datatypes.py
+++ koila/interfaces/components/datatypes.py
@@ -7,5 +7,14 @@ from torch import dtype as DType
 
 
 class DataType(Protocol):
+    """
+    `DataType` represents a tensor's datatype.
+    A tensor's datatype has two attributes we care about,
+    its width in memory (`dtype`), and the device it's on (`device`).
+    """
+    
     dtype: DType
+    "The datatype of a tensor."
+
     device: str | Device
+    "The device the tensor is on."
--- koila/interfaces/components/indexible.py
+++ koila/interfaces/components/indexible.py
@@ -1,14 +1,26 @@
 from __future__ import annotations
 
 from abc import abstractmethod
-from typing import Any, Protocol
+from typing import Protocol, TypeVar
+
+K = TypeVar("K")
+V = TypeVar("V")
 
 
-class Indexible(Protocol):
+class Indexible(Protocol[K, V]):
+    """
+    An `Indexible` container acts like a dictionary, that can be looked up.
+    A list is indexible (with its key type limited to integers.)
+    """
+
     @abstractmethod
-    def __getitem__(self, index: Any) -> Any:
+    def __getitem__(self, index: K) -> V:
+        "The [] (getter) operator."
+
         ...
 
     @abstractmethod
-    def __setitem__(self, index: Any, value: Any) -> None:
+    def __setitem__(self, index: K, value: V) -> None:
+        "The [] (setter) operator."
+
         ...
