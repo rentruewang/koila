6307337bbc874b615477cc138064f0d70aada38d
RenChu Wang <patrick1031wang@gmail.com>
2025-09-16 20:59:25 -0700

    🖼️ `Frame` related changes.

--- tests/ops/test_maps.py
+++ tests/ops/test_maps.py
@@ -26,10 +26,10 @@ def filter_op(request) -> Op:
     return request.param()
 
 
-def test_filter(filter_op, block_frame_op, make_executor):
+def test_filter(filter_op, block_frame, make_executor):
     for filtered, original in zip(
-        make_executor(filter_op.thunk(block_frame_op.thunk())),
-        make_executor(block_frame_op.thunk()),
+        make_executor(filter_op.thunk(block_frame.op.thunk())),
+        make_executor(block_frame.op.thunk()),
     ):
         original_filtered = batches.tensordict_filter(original, "f1d > 0")
         assert (filtered == original_filtered).all()
@@ -40,11 +40,11 @@ def renames():
     return {"f1d": "f1", "f2d": "f2", "i1d": "i1", "i2d": "i2"}
 
 
-def test_rename(block_frame_op, renames, make_executor):
+def test_rename(block_frame, renames, make_executor):
     rename_op = RenameOp(renames)
     for renamed, original in zip(
-        make_executor(rename_op.thunk(block_frame_op.thunk())),
-        make_executor(block_frame_op.thunk()),
+        make_executor(rename_op.thunk(block_frame.op.thunk())),
+        make_executor(block_frame.op.thunk()),
     ):
         original_renamed = batches.tensordict_rename(
             original, f1d="f1", f2d="f2", i1d="i1", i2d="i2"
@@ -57,20 +57,20 @@ def map_rename():
     return {"f1d": "f", "i1d": "i"}
 
 
-def test_func_op(block_frame_op, map_rename, make_executor):
+def test_func_op(block_frame, map_rename, make_executor):
     f = lambda b: batches.tensordict_rename(b, **map_rename)
     func_op = FuncOp(func=f)
     for mapped, original in zip(
-        make_executor(func_op.thunk(block_frame_op.thunk())),
-        make_executor(block_frame_op.thunk()),
+        make_executor(func_op.thunk(block_frame.op.thunk())),
+        make_executor(block_frame.op.thunk()),
     ):
         assert (mapped == f(original)).all()
 
 
-def test_project(block_frame_op, make_executor):
+def test_project(block_frame, make_executor):
     project_op = ProjectOp(subset=["f1d", "i2d"])
     for curr, other in zip(
-        make_executor(project_op.thunk(block_frame_op.thunk())),
-        make_executor(block_frame_op.thunk()),
+        make_executor(project_op.thunk(block_frame.op.thunk())),
+        make_executor(block_frame.op.thunk()),
     ):
         assert (curr == other.select("f1d", "i2d")).all()
