e0a69e24a4bb5658e02492ab374567e868810bdd
RenChu Wang <patrick1031wang@gmail.com>
2026-01-19 21:07:43 -0800

    🧨 Deleted the deprecated module.

--- src/aioway/_deprecated/attrs/dtypes/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) AIoWay Authors - All Rights Reserved
-
-from .dtypes import *
-from .factories import *
-from .primitives import *
--- src/aioway/_deprecated/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) AIoWay Authors - All Rights Reserved
-
-from .parsers import *
-from .schemas import *
