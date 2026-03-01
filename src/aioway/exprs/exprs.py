048c4c2df413eb3172dfe3ba5e707e0865029f6e
RenChu Wang <patrick1031wang@gmail.com>
2026-02-28 22:00:38 -0800

    🐼 Column expression like `pd.col` (#166)
    Simplify column expressions, do not use `.subs` as that is not too pythonic.
    When using `.subs`, no type hints / completion can be had because it's not python, hurting developer experience.
    Furthermore, it requires passing a global set of variables, which is inconvenient.

--- src/aioway/exprs/exprs.py
+++ src/aioway/exprs/exprs.py
@@ -3,8 +3,9 @@
 import abc
 import logging
 import typing
-from collections.abc import Iterator, Sequence
-from typing import ClassVar, Literal, Protocol
+from abc import ABC
+from collections.abc import Iterator, KeysView, Sequence
+from typing import ClassVar, Literal
 
 from aioway.tables import Table
 
@@ -13,7 +14,7 @@ __all__ = ["Expr", "ColumnExpr", "TableExpr"]
 LOGGER = logging.getLogger(__name__)
 
 
-class Expr(Protocol):
+class Expr(ABC):
     """
     An (extended) projection operator that can be reprsented as an expression.
     """
@@ -48,19 +49,128 @@ class Expr(Protocol):
         ...
 
 
-class ColumnExpr(Expr, Protocol):
+class ColumnExpr(Expr, ABC):
     @abc.abstractmethod
-    def subs[C](self, **tables: Table[C]) -> C: ...
+    def __str__(self) -> str: ...
+
+    @typing.final
+    def __add__(self, other: "ColumnExpr"):
+        from .ufuncs import AddColExpr
+
+        return AddColExpr(self, other)
+
+    @typing.final
+    def __radd__(self, other: "ColumnExpr"):
+        return other + self
+
+    @typing.final
+    def __sub__(self, other: "ColumnExpr"):
+        from .ufuncs import SubColExpr
+
+        return SubColExpr(self, other)
+
+    @typing.final
+    def __rsub__(self, other: "ColumnExpr"):
+        return other - self
+
+    @typing.final
+    def __mul__(self, other: "ColumnExpr"):
+        from .ufuncs import MultColExpr
+
+        return MultColExpr(self, other)
+
+    @typing.final
+    def __rmul__(self, other: "ColumnExpr"):
+        return other * self
+
+    @typing.final
+    def __truediv__(self, other: "ColumnExpr"):
+        from .ufuncs import TrueDivColExpr
+
+        return TrueDivColExpr(self, other)
+
+    @typing.final
+    def __rtruediv__(self, other: "ColumnExpr"):
+        return other / self
+
+    @typing.final
+    def __floordiv__(self, other: "ColumnExpr"):
+        from .ufuncs import FloorDivColExpr
+
+        return FloorDivColExpr(self, other)
+
+    @typing.final
+    def __rfloordiv__(self, other: "ColumnExpr"):
+        return other // self
+
+    @typing.final
+    def __pow__(self, other: "ColumnExpr"):
+        from .ufuncs import ExpColExpr
+
+        return ExpColExpr(self, other)
+
+    @typing.final
+    def __rpow__(self, other: "ColumnExpr"):
+        return other**self
+
+    @typing.final
+    def __eq__(self, other: object):
+        if isinstance(other, ColumnExpr):
+            from .ufuncs import EqColExpr
+
+            return EqColExpr(self, other)
+
+        return NotImplemented
+
+    @typing.final
+    def __ne__(self, other: object):
+        if isinstance(other, ColumnExpr):
+            from .ufuncs import NeColExpr
+
+            return NeColExpr(self, other)
+
+        return NotImplemented
+
+    @typing.final
+    def __gt__(self, other: "ColumnExpr"):
+        from .ufuncs import GtColExpr
+
+        return GtColExpr(self, other)
+
+    @typing.final
+    def __ge__(self, other: "ColumnExpr"):
+        from .ufuncs import GeColExpr
+
+        return GeColExpr(self, other)
+
+    @typing.final
+    def __lt__(self, other: "ColumnExpr"):
+        from .ufuncs import LtColExpr
+
+        return LtColExpr(self, other)
+
+    @typing.final
+    def __le__(self, other: "ColumnExpr"):
+        from .ufuncs import LeColExpr
+
+        return LeColExpr(self, other)
+
+
+class TableExpr(Expr, Table[ColumnExpr], ABC):
+    @abc.abstractmethod
+    def __str__(self) -> str: ...
+
+    @abc.abstractmethod
+    def keys(self) -> KeysView[str]: ...
 
     @typing.override
-    def __repr__(self) -> str:
-        return f"col({self!s})"
+    def column(self, key: str) -> ColumnExpr:
+        from .cols import GetItemExpr
 
-
-class TableExpr(Expr, Protocol):
-    @abc.abstractmethod
-    def subs[C](self, **tables: Table[C]) -> Table[C]: ...
+        return GetItemExpr(table=self, column=key)
 
     @typing.override
-    def __repr__(self) -> str:
-        return f"sel({self!s})"
+    def select(self, *keys: str) -> "TableExpr":
+        from .tables import SelectExpr
+
+        return SelectExpr(table=self, columns=keys)
