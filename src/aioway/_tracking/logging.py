fcf28382ea88c1020e2b5316c804f3e640da75e2
RenChu Wang <patrick1031wang@gmail.com>
2026-03-15 00:53:05 -0700

    track file

--- src/aioway/_tracking/logging.py
+++ src/aioway/_tracking/logging.py
@@ -1,26 +1,28 @@
 # Copyright (c) AIoWay Authors - All Rights Reserved
 
+"Turning on logging globally for a duration."
+
 import contextlib as ctxl
 import logging
 from logging import Handler
 
 from rich.logging import RichHandler
 
-__all__ = ["enable_logging", "enable_rich_logging"]
+__all__ = ["enable", "enable_rich"]
 
 
 @ctxl.contextmanager
-def enable_rich_logging(level: str | int, /):
+def enable_rich(level: str | int, /):
     """
     Enable logging for the duration of the block with rich handlers.
     """
 
-    with enable_logging(level, RichHandler()) as logger:
+    with enable(level, RichHandler()) as logger:
         yield logger
 
 
 @ctxl.contextmanager
-def enable_logging(level: str | int, /, *handlers: Handler):
+def enable(level: str | int, /, *handlers: Handler):
     """
     Enable logging for the duration of the block package wide.
     """
---
+++ src/aioway/_tracking/tracking.py
@@ -0,0 +1,10 @@
+# Copyright (c) AIoWay Authors - All Rights Reserved
+
+import contextlib as ctxl
+
+__all__ = []
+
+
+@ctxl.contextmanager
+def track():
+    raise NotImplementedError
