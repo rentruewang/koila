d5c0c6e207a6bc3e67a4a662c13ecbe8d346f4c9
RenChu Wang <patrick1031wang@gmail.com>
2026-03-19 00:48:45 -0700

    nn in features

---
+++ tests/nn/dense.py
@@ -0,0 +1,47 @@
+# Copyright (c) AIoWay Authors - All Rights Reserved
+
+"The dense layers from `torch.nn`."
+
+from torch.nn import Linear as _Linear
+import dataclasses as dcls
+import functools
+from aioway.attrs import Attr
+from aioway import _logging
+
+__all__ = ["Linear"]
+
+LOGGER = _logging.get_logger(__name__)
+
+
+class Linear:
+    """
+    The wrapper for `torch.nn.Linear`.
+    """
+
+    def __init__(self, in_features: int, out_features: int, bias: bool = True) -> None:
+        LOGGER.info(
+            "Constructing `Linear(in_features=%s, out_features=%s, bias=%s)`",
+            in_features,
+            out_features,
+            bias,
+        )
+
+        self._in_features = in_features
+        self._out_features = out_features
+        self._bias = bias
+
+    @functools.cached_property
+    def _model(self):
+        return _Linear(
+            in_features=self._in_features,
+            out_features=self._out_features,
+            bias=self._bias,
+        )
+
+    @property
+    def in_attrs(self):
+        raise NotImplementedError
+
+    @property
+    def out_attrs(self):
+        raise NotImplementedError
