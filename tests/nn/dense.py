27c7fc4eb5f5f40df3fc3370dea8961c03cc60fc
RenChu Wang <patrick1031wang@gmail.com>
2026-03-21 23:41:25 -0700

    linear layer ok

--- tests/nn/dense.py
+++ tests/nn/dense.py
@@ -4,9 +4,11 @@
 
 import functools
 
+from torch import Tensor
 from torch.nn import Linear as _Linear
 
 from aioway import _logging
+from aioway.attrs import Attr
 
 __all__ = ["Linear"]
 
@@ -18,7 +20,14 @@ class Linear:
     The wrapper for `torch.nn.Linear`.
     """
 
-    def __init__(self, in_features: int, out_features: int, bias: bool = True) -> None:
+    def __init__(
+        self,
+        *,
+        in_features: int,
+        out_features: int,
+        bias: bool = True,
+        device: str = "cpu",
+    ) -> None:
         LOGGER.info(
             "Constructing `Linear(in_features=%s, out_features=%s, bias=%s)`",
             in_features,
@@ -29,6 +38,26 @@ class Linear:
         self._in_features = in_features
         self._out_features = out_features
         self._bias = bias
+
+        self._device = device
+        self._dtype = "float32"
+
+    def forward(self, data: Tensor) -> Tensor:
+        return self._model(data)
+
+    def attr(self, input_attr: Attr) -> Attr:
+        if input_attr.device != self._device:
+            return NotImplemented
+
+        if input_attr.shape[-1] != self._in_features:
+            return NotImplemented
+
+        shape = [input_attr.shape[:-1], self._out_features]
+        return Attr.parse(
+            device=input_attr.device,
+            dtype=(input_attr.dtype.term * self._dtype).unpack(),
+            shape=shape,
+        )
 
     @functools.cached_property
     def _model(self):
